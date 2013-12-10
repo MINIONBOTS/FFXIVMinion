1997e82bb2b59eab94b7544cfb43f45ef2b9446b
f3renz <nathan.wingert@gmail.com>
2013-12-10 10:05:45 -0700

    fixing beta repo

---
+++ minionlib/ml_blacklist_mgr.lua
@@ -0,0 +1,10 @@
+ml_blacklist_mgr = {}
+ml_blacklist_mgr.mainwindow = { name = strings[gCurrentLanguage].blacklistManager, x = 350, y = 100, w = 250, h = 400}
+
+ml_blacklist_mgr.HandleInit()
+    GUI_NewWindow(ml_blacklist_mgr.mainwindow.name,ml_blacklist_mgr.mainwindow.x,ml_blacklist_mgr.mainwindow.y,ml_blacklist_mgr.mainwindow.w,ml_blacklist_mgr.mainwindow.h)
+    
+    for 
+    
+end
+
