6b1b17bb075cb7bbc2091218c6ed1c70c0edee4a
aceRage <ace.savell@gmail.com>
2014-10-26 09:48:15 -0500

    new file for aoe avoidance

---
+++ ffxivminion/ffxiv_aoe_data.lua
@@ -0,0 +1,32 @@
+ffxiv_aoe_data = 
+{
+	["circle"] = {
+		--Labored Leap
+		[2809] = true,
+		[1130] = true,
+		[1006] = true,
+		--Seismic Rift
+		[2818] = true,
+		[547] = true,
+		[546] = true,
+		--Plaincracker
+		[2786] = true,
+		[1682] = true,
+		[1087] = true,
+		[482] = true,
+		--Detonator
+		[1832] = true,
+		[1007] = true,
+		
+		[497] = true,
+		[1010] = true,
+		[1860] = true,
+		[2090] = true,
+		[2197] = true,
+		[1016] = true,
+		[503] = true,
+	},
+	["frontal"] = {
+	
+	}
+}
