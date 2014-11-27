200a9dc51cdc2da41dbab9bfed34529883d2e2c8
aceRage <ace.savell@gmail.com>
2014-11-27 10:51:05 -0600

    update crafting with max craft amount

--- ffxivminion/ffxiv_task_craft.lua
+++ ffxivminion/ffxiv_task_craft.lua
@@ -13,56 +13,88 @@ function ffxiv_task_craft.Create()
     newinst.overwatch_elements = {}
     
     --ffxiv_task_craft members
-    e_opencraftwnd.CraftStart = ml_global_information.Now
 	newinst.networkLatency = 0
+	newinst.synthStarted = false
+	newinst.attemptedStarts = 0
+	newinst.itemsCrafted = 0
+	newinst.maxItems = tonumber(gCraftMaxItems)
     
     return newinst
 end
 
-c_opencraftwnd = inheritsFrom( ml_cause )
-e_opencraftwnd  = inheritsFrom( ml_effect )
-c_opencraftwnd.throttle = 1500
-function c_opencraftwnd:evaluate()
+c_craftlimit = inheritsFrom( ml_cause )
+e_craftlimit  = inheritsFrom( ml_effect )
+function c_craftlimit:evaluate()
+	if ( Now() < ml_task_hub:ThisTask().networkLatency ) then
+		return false
+	end
+	
     local synth = Crafting:SynthInfo()
-	if (  ml_global_information.Now - e_opencraftwnd.CraftStart > 3000 and  not synth and not Crafting:IsCraftingLogOpen()) then
+	if ( not synth and
+		((ml_task_hub:ThisTask().maxItems > 0 and ml_task_hub:ThisTask().itemsCrafted == ml_task_hub:ThisTask().maxItems) or ml_task_hub:ThisTask().attemptedStarts > 2)) 
+	then
 		return true
 	end	
+	
+    return false
+end
+function e_craftlimit:execute()
+	ml_task_hub:ToggleRun()
+end
+
+c_opencraftwnd = inheritsFrom( ml_cause )
+e_opencraftwnd  = inheritsFrom( ml_effect )
+function c_opencraftwnd:evaluate()
+	if ( Now() < ml_task_hub:ThisTask().networkLatency ) then
+		return false
+	end
+	
+    local synth = Crafting:SynthInfo()
+	if ( not synth and not Crafting:IsCraftingLogOpen() ) then
+		return true
+	end	
+	
     return false
 end
 
 function e_opencraftwnd:execute()
     Crafting:ToggleCraftingLog()
+	ml_task_hub:ThisTask().networkLatency = Now() + 1500
 end
 
 c_selectitem = inheritsFrom( ml_cause )
 e_selectitem = inheritsFrom( ml_effect )
-c_selectitem.throttle = 1000
 function c_selectitem:evaluate()
-	if (Now() < ml_task_hub:CurrentTask().networkLatency or Player.cp.current < tonumber(gCraftMinCP)) then
+	if (Now() < ml_task_hub:ThisTask().networkLatency or Player.cp.current < tonumber(gCraftMinCP)) then
 		return false
 	end
-	
+
     local synth = Crafting:SynthInfo()
 	if ( not synth and Crafting:IsCraftingLogOpen()) then
-		d("Can craft selected Item? : "..tostring(Crafting:CanCraftSelectedItem()))
 		if ( Crafting:CanCraftSelectedItem() ) then
 			return true
 		end
 	end	
+	
     return false
 end
 function e_selectitem:execute()
 	Crafting:CraftSelectedItem()
 	Crafting:ToggleCraftingLog()
-	e_opencraftwnd.CraftStart = ml_global_information.Now
+	ml_task_hub:ThisTask().attemptedStarts = ml_task_hub:ThisTask().attemptedStarts + 1
 	SkillMgr.currentIQStack = 0 
 	SkillMgr.lastquality = 0
+	ml_task_hub:ThisTask().networkLatency = Now() + 1500
 end
 
 c_precraftbuff = inheritsFrom( ml_cause )
 e_precraftbuff = inheritsFrom( ml_effect )
 e_precraftbuff.id = 0
 function c_precraftbuff:evaluate()
+	if ( Now() < ml_task_hub:ThisTask().networkLatency ) then
+		return false
+	end
+	
 	local synth = Crafting:SynthInfo()	
 	if (not synth) then
 		Repair()
@@ -103,11 +135,13 @@ function e_precraftbuff:execute()
 	end
 end
 
-
 c_craft = inheritsFrom( ml_cause )
 e_craft = inheritsFrom( ml_effect )
-e_craft.throttle = 1000
 function c_craft:evaluate()
+	if ( Now() < ml_task_hub:ThisTask().networkLatency ) then
+		return false
+	end
+
     local synth = Crafting:SynthInfo()
 	if ( synth ) then		
 		return true	
@@ -115,12 +149,20 @@ function c_craft:evaluate()
     return false
 end
 function e_craft:execute()
+	if (ml_task_hub:ThisTask().attemptedStarts > 0) then
+		ml_task_hub:ThisTask().attemptedStarts = 0
+		ml_task_hub:ThisTask().synthStarted = true
+		ml_task_hub:ThisTask().itemsCrafted = ml_task_hub:ThisTask().itemsCrafted + 1
+	end
     SkillMgr.Craft()
+	ml_task_hub:ThisTask().networkLatency = Now() + 1000
 end
 
 function ffxiv_task_craft:Init()
     --init Process() cnes
-    	
+	local ke_reachedCraftLimit = ml_element:create( "ReachedCraftLimit", c_craftlimit, e_craftlimit, 25 )
+    self:add(ke_reachedCraftLimit, self.process_elements)
+	
 	local ke_precraftbuff = ml_element:create( "PreCraftBuff", c_precraftbuff, e_precraftbuff, 20 )
     self:add(ke_precraftbuff, self.process_elements)
 
@@ -146,6 +188,9 @@ function ffxiv_task_craft.UIInit()
 	if ( Settings.FFXIVMINION.gCraftMinCP == nil ) then
         Settings.FFXIVMINION.gCraftMinCP = 0
     end
+	if ( Settings.FFXIVMINION.gCraftMaxItems == nil ) then
+        Settings.FFXIVMINION.gCraftMaxItems = 0
+    end
 	
 	local winName = GetString("craftMode")
 	GUI_NewButton(winName, ml_global_information.BtnStart.Name , ml_global_information.BtnStart.Event)
@@ -156,20 +201,25 @@ function ffxiv_task_craft.UIInit()
 	GUI_NewComboBox(winName,strings[gCurrentLanguage].skillProfile,"gSMprofile",group,ffxivminion.Strings.SKMProfiles())
     GUI_NewCheckbox(winName,strings[gCurrentLanguage].botEnabled,"gBotRunning",group)
 	local group = GetString("settings")
+	GUI_NewField(winName,"Craft Amount","gCraftMaxItems",group)
     GUI_NewField(winName,"Minimum CP","gCraftMinCP",group)
 	
 	GUI_UnFoldGroup(winName,GetString("status"))
+	GUI_UnFoldGroup(winName,GetString("settings"))
 	ffxivminion.SizeWindow(winName)
 	GUI_WindowVisible(winName, false)
 	
 	gCraftMinCP = Settings.FFXIVMINION.gCraftMinCP
-    
+    gCraftMaxItems = Settings.FFXIVMINION.gCraftMaxItems
+	
     RegisterEventHandler("GUI.Update",ffxiv_task_craft.GUIVarUpdate)
 end
 
 function ffxiv_task_craft.GUIVarUpdate(Event, NewVals, OldVals)
     for k,v in pairs(NewVals) do
-        if 	( k == "gCraftMinCP" ) then
+        if 	( 	k == "gCraftMinCP" or 
+				k == "gCraftMaxItems" ) 
+		then
             Settings.FFXIVMINION[tostring(k)] = v
         end
     end
