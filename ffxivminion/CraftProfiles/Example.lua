7cfe949325c30a38de7d51d339fbb29a41b4d24d
aceRage <ace.savell@gmail.com>
2015-10-21 12:46:08 -0500

    Example file, to create directory.

---
+++ ffxivminion/CraftProfiles/Example.lua
@@ -0,0 +1,39 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["orders"] = {
+		[1008] = {
+			["amount"] = 4;
+			["class"] = 0;
+			["counthq"] = true;
+			["id"] = 1008;
+			["index"] = 0;
+			["item"] = 5361;
+			["level"] = 1;
+			["name"] = "Maple Lumber";
+			["page"] = 0;
+			["profile"] = "";
+			["requirehq"] = false;
+			["usehq"] = false;
+			["usequick"] = true;
+		};
+		[1009] = {
+			["amount"] = 2;
+			["class"] = 0;
+			["counthq"] = false;
+			["id"] = 1009;
+			["index"] = 1;
+			["item"] = 3742;
+			["level"] = 1;
+			["name"] = "Maple Clogs";
+			["page"] = 0;
+			["profile"] = "Craft_BasicFinisher";
+			["requirehq"] = false;
+			["usehq"] = false;
+			["usequick"] = false;
+		};
+	};
+}
+return obj1
