2eb7b0a913fa354b3db1c5aeff0105b5a9ebee3e
fxfire <s.karsten@scenix3d.com>
2016-03-10 11:48:11 +0100

    EorzeaTime workaround fix

--- ffxivminion/ffxiv.lua
+++ ffxivminion/ffxiv.lua
@@ -527,7 +527,8 @@ function ml_global_information.InGameOnUpdate( event, tickcount )
 			gStatusMarkerTime = ""
 		end
 		
-		local et = AceLib.API.Weather.GetDateTime() 
+		--local et = AceLib.API.Weather.GetDateTime()   y u no test!?
+		local et = EorzeaTime() 
 		gEorzeaTime = tostring(et.hour)..":"..(et.minute < 10 and "0" or "")..tostring(et.minute)
 		
 		-- Mesher.lua
