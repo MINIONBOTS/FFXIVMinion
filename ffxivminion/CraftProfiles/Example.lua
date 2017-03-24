4274a24344ba998dde908a45db7fe40a1ab414f4
Ace <ace.savell@gmail.com>
2017-03-23 21:55:49 -0500

    Add the example to create the base directory.

---
+++ ffxivminion/CraftProfiles/Example.lua
@@ -0,0 +1,71 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["orders"] = {
+		[1008] = {
+			["amount"] = 4;
+			["class"] = 0;
+			["completed"] = false;
+			["counthq"] = true;
+			["hq1"] = 0;
+			["hq1max"] = false;
+			["hq2"] = 0;
+			["hq2max"] = false;
+			["hq3"] = 0;
+			["hq3max"] = false;
+			["hq4"] = 0;
+			["hq4max"] = false;
+			["hq5"] = 0;
+			["hq5max"] = false;
+			["hq6"] = 0;
+			["hq6max"] = false;
+			["id"] = 1008;
+			["index"] = 0;
+			["item"] = 5361;
+			["level"] = 1;
+			["name"] = "Maple Lumber";
+			["page"] = 0;
+			["profile"] = "";
+			["requiredfood"] = "None";
+			["requirehq"] = false;
+			["skillprofile"] = "None";
+			["skip"] = false;
+			["usehq"] = true;
+			["usequick"] = true;
+		};
+		[1009] = {
+			["amount"] = 2;
+			["class"] = 0;
+			["completed"] = false;
+			["counthq"] = false;
+			["hq1"] = 1;
+			["hq1max"] = true;
+			["hq2"] = 0;
+			["hq2max"] = false;
+			["hq3"] = 0;
+			["hq3max"] = false;
+			["hq4"] = 0;
+			["hq4max"] = false;
+			["hq5"] = 0;
+			["hq5max"] = false;
+			["hq6"] = 0;
+			["hq6max"] = false;
+			["id"] = 1009;
+			["index"] = 1;
+			["item"] = 3742;
+			["level"] = 1;
+			["name"] = "Maple Clogs";
+			["page"] = 0;
+			["profile"] = "Craft_BasicFinisher";
+			["requiredfood"] = "None";
+			["requirehq"] = false;
+			["skillprofile"] = "None";
+			["skip"] = false;
+			["usehq"] = true;
+			["usequick"] = false;
+		};
+	};
+}
+return obj1
