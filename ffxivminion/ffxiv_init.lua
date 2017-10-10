dd5429c83cf7bb8e197960cc73243c882b2f8053
Ace <ace.savell@gmail.com>
2017-10-09 20:11:42 -0500

    Revert "Rollback for login issues."
    This reverts commit db8d7047efc168bd33b4ff817700817e48300d12.

--- ffxivminion/ffxiv_init.lua
+++ ffxivminion/ffxiv_init.lua
@@ -505,14 +505,27 @@ function ml_global_information.Init()
 end
 
 function IsControlOpen(strControl)
+	if (memoize and memoize.opencontrols) then
+		if (memoize.opencontrols[strControl]) then
+			return (memoize.opencontrols[strControl] == true)
+		end
+	end
+
+	if (memoize.opencontrols == nil) then
+		memoize.opencontrols = {}
+	end
+	
 	local controls = GetControls()
 	if (table.valid(controls)) then
 		for id,e in pairs(controls) do
 			if (e.name == strControl) then
-				return e:IsOpen()
-			end
+				local isopen = e:IsOpen()
+				memoize.opencontrols[strControl] = isopen
+				return isopen
+			end				
 		end
 	end
+		
 	return false
 end
 
