50082e1d6211029a7dd5209badc7cfeb2effb46a
Ace <ace.savell@gmail.com>
2018-05-10 14:33:03 -0500

    make sure pathindex is initialized, added cutscene notifiers

--- ffxivminion/ffxiv_common_cne.lua
+++ ffxivminion/ffxiv_common_cne.lua
@@ -4057,7 +4057,7 @@ function c_skipcutscene:evaluate()
 			end
 		end
 		
-		if (In(totalUI,4725,5701,3451,2628) and not IsControlOpen("NowLoading")) then
+		if (In(totalUI,4725,5701,3451,2628,2626,2893) and not IsControlOpen("NowLoading")) then
 			if (IsControlOpen("SelectString") or IsControlOpen("SelectIconString")) then
 				local convoList = GetConversationList()
 				if (table.valid(convoList)) then
