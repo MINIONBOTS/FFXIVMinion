f8eeced3a3dd4336412e17f9d4680b54c2ab1c6e
neonguy <SebbsSoftware@westnet.com.au>
2019-07-09 20:29:53 +0800

    no message

--- ffxivminion/ffxiv_common_cne.lua
+++ ffxivminion/ffxiv_common_cne.lua
@@ -1004,7 +1004,8 @@ function c_teleporttomap:evaluate()
 				e_teleporttomap.aeth = aeth
 				return true
 			end
-			
+			end
+			local attunedAetherytes = GetAttunedAetheryteList()
 			-- Fall back check to see if we can get to EL, and from there to the destination.
 			for k,aetheryte in pairs(attunedAetherytes) do
 				if (aetheryte.id == 134 and GilCount() >= aetheryte.price) then
@@ -1016,8 +1017,7 @@ function c_teleporttomap:evaluate()
 						return true
 					end
 				end
-			end
-			local attunedAetherytes = GetAttunedAetheryteList()
+			
 			-- Fall back check to see if we can get to Crystal, and from there to the destination.
 			for k,aetheryte in pairs(attunedAetherytes) do
 				if (aetheryte.id == 133 and GilCount() >= aetheryte.price) then
