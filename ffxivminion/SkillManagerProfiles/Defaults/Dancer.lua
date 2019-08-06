be4736933214c31274347d291d81393b5d38ab87
Ace <ace.savell@gmail.com>
2019-08-06 08:52:03 -0500

    basic dancer, no dances yet

---
+++ ffxivminion/SkillManagerProfiles/Defaults/Dancer.lua
@@ -0,0 +1,204 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	["classes"] = {
+		[1] = false;
+		[2] = false;
+		[3] = false;
+		[4] = true;
+		[5] = false;
+		[6] = false;
+		[7] = false;
+		[8] = false;
+		[9] = false;
+		[10] = false;
+		[11] = false;
+		[12] = false;
+		[13] = false;
+		[14] = false;
+		[15] = false;
+		[16] = false;
+		[17] = false;
+		[18] = false;
+		[19] = false;
+		[20] = false;
+		[21] = false;
+		[22] = true;
+		[23] = false;
+		[24] = false;
+		[25] = false;
+		[26] = false;
+		[27] = false;
+		[28] = false;
+		[29] = false;
+		[30] = false;
+		[31] = false;
+		[32] = false;
+		[33] = false;
+		[34] = false;
+		[35] = false;
+		[36] = false;
+		[37] = false;
+		[38] = false;
+	};
+	["filters"] = {
+		[1] = "Jumps Off";
+		[2] = "Aoe Off";
+		[3] = "Stuns Off";
+		[4] = "Buffs Off";
+		[5] = "Battle Litany Off";
+	};
+	["skills"] = {
+		[1] = {
+			["id"] = 15996;
+			["levelmin"] = 45;
+			["maxRange"] = 0;
+			["name"] = "Bloodshower";
+			["prio"] = 1;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[2] = {
+			["id"] = 15995;
+			["levelmin"] = 35;
+			["maxRange"] = 0;
+			["name"] = "Rising Windmill";
+			["prio"] = 2;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[3] = {
+			["id"] = 15992;
+			["levelmin"] = 40;
+			["maxRange"] = 25;
+			["name"] = "Fountainfall";
+			["prio"] = 3;
+		};
+		[4] = {
+			["id"] = 15991;
+			["levelmin"] = 20;
+			["maxRange"] = 25;
+			["name"] = "Reverse Cascade";
+			["prio"] = 4;
+		};
+		[5] = {
+			["id"] = 15996;
+			["levelmin"] = 45;
+			["maxRange"] = 0;
+			["name"] = "Bloodshower";
+			["prio"] = 5;
+			["tarange"] = 5;
+			["tecount"] = 1;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[6] = {
+			["id"] = 15995;
+			["levelmin"] = 35;
+			["maxRange"] = 0;
+			["name"] = "Rising Windmill";
+			["prio"] = 6;
+			["tarange"] = 5;
+			["tecount"] = 1;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[7] = {
+			["id"] = 15994;
+			["levelmin"] = 25;
+			["maxRange"] = 0;
+			["name"] = "Bladeshower";
+			["pcskill"] = "15993";
+			["prio"] = 7;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[8] = {
+			["id"] = 15993;
+			["levelmin"] = 15;
+			["maxRange"] = 0;
+			["name"] = "Windmill";
+			["prio"] = 8;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[9] = {
+			["id"] = 15989;
+			["levelmin"] = 1;
+			["maxRange"] = 25;
+			["name"] = "Cascade";
+			["pcskill"] = "15990";
+			["prio"] = 9;
+		};
+		[10] = {
+			["id"] = 15990;
+			["levelmin"] = 2;
+			["maxRange"] = 25;
+			["name"] = "Fountain";
+			["prio"] = 10;
+		};
+		[11] = {
+			["id"] = 16011;
+			["levelmin"] = 62;
+			["maxRange"] = 0;
+			["name"] = "Devilment";
+			["prio"] = 11;
+		};
+		[12] = {
+			["id"] = 16005;
+			["levelmin"] = 76;
+			["maxRange"] = 25;
+			["name"] = "Saber Dance";
+			["prio"] = 16;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[13] = {
+			["id"] = 16008;
+			["levelmin"] = 50;
+			["maxRange"] = 0;
+			["name"] = "Fan Dance II";
+			["prio"] = 13;
+			["tarange"] = 5;
+			["tecount"] = 2;
+			["terange"] = 5;
+			["trg"] = "Player";
+		};
+		[14] = {
+			["id"] = 16007;
+			["levelmin"] = 30;
+			["maxRange"] = 25;
+			["name"] = "Fan Dance";
+			["prio"] = 12;
+		};
+		[15] = {
+			["gcd"] = "True";
+			["id"] = 16009;
+			["levelmin"] = 66;
+			["maxRange"] = 25;
+			["name"] = "Fan Dance III";
+			["prio"] = 14;
+			["tarange"] = 5;
+			["terange"] = 5;
+		};
+		[16] = {
+			["id"] = 16013;
+			["levelmin"] = 72;
+			["maxRange"] = 0;
+			["name"] = "Flourish";
+			["prio"] = 15;
+		};
+	};
+	["version"] = 3;
+}
+return obj1
