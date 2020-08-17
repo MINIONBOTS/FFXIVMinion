aa3e8ee33cada42393d04cca063b54b1d3def629
xeno002 <xeno.welsh@gmail.com>
2020-08-17 19:20:47 +0200

    added 24man p5.3 "The Puppets' Bunker"
    SetDefaultMesh(917, "[Raid] The Puppets' Bunker", enforce

--- ffxivminion/ffxiv_init.lua
+++ ffxivminion/ffxiv_init.lua
@@ -628,7 +628,7 @@ function ml_global_information.Init()
 		-- Raid Alliance SHB
 		ml_mesh_mgr.SetDefaultMesh(882, "[Raid] The Copied Factory", enforce)
 		ml_mesh_mgr.SetDefaultMesh(896, "[Raid] The Copied Factory", enforce)
-		--ml_mesh_mgr.SetDefaultMesh(???, "[Raid] ???", enforce)
+		ml_mesh_mgr.SetDefaultMesh(917, "[Raid] The Puppets' Bunker", enforce)
 		--ml_mesh_mgr.SetDefaultMesh(???, "[Raid] ???", enforce)
 		
 		-- Raids ARR
