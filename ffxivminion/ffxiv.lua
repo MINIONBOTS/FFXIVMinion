c2158a90daa5ad55bcd218b33018f9d6b905ad9b
neonguy <SebbsSoftware@westnet.com.au>
2021-01-12 14:46:31 +0800

    5.41 patch update check

--- ffxivminion/ffxiv.lua
+++ ffxivminion/ffxiv.lua
@@ -13,7 +13,7 @@ ffxivminion.AetherCurrentCompleted = false
 ffxivminion.DutyCurrentData = {}
 ffxivminion.gameRegion = GetGameRegion()
 ffxivminion.patchLevel = {
-	[1] = 5.4,
+	[1] = 5.41,
 	[2] = 5.31,
 	[3] = 5.25
 }
