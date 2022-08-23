06c16cd7f3f51fc72bd5392310f9066524f53d11
Sebbs <SebbsSoftware@westnet.com.au>
2022-08-23 19:36:06 +0800

    no message

--- ffxivminion/ffxiv.lua
+++ ffxivminion/ffxiv.lua
@@ -21,7 +21,7 @@ ffxivminion.AetherCurrentCompleted = false
 ffxivminion.DutyCurrentData = {}
 ffxivminion.gameRegion = GetGameRegion()
 ffxivminion.patchLevel = {
-	[1] = 6.18,
+	[1] = 6.2,
 	[2] = 6.0,
 	[3] = 6.0
 }
