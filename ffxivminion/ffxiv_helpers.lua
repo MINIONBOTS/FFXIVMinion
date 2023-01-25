7cd9ca9487ee711930b0d0a10f530240b13cbdf7
Sebbs <SebbsSoftware@westnet.com.au>
2023-01-25 14:55:30 +0800

    new map type for helpers

--- ffxivminion/ffxiv_helpers.lua
+++ ffxivminion/ffxiv_helpers.lua
@@ -3428,7 +3428,7 @@ function IsMap(itemid)
 		(itemid == 7884 or itemid == 8156 or itemid == 9900) or
 		(itemid >= 12241 and itemid <= 12243) or
 		(itemid >= 17835 and itemid <= 17836) or 
-		In(itemid,19770,24794,26744,26745,33611,36612))
+		In(itemid,19770,24794,26744,26745,33611,36612,39591))
 end
 function IsGardening(itemid)
 	local itemid = tonumber(itemid) or 0
