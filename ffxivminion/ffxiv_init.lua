0ff45d71f59586c78b58e57bf3c6ebbc849ac966
Sebbs <SebbsSoftware@westnet.com.au>
2025-03-26 08:18:27 +0800

    latty nav stuff

--- ffxivminion/ffxiv_init.lua
+++ ffxivminion/ffxiv_init.lua
@@ -1160,6 +1160,7 @@ function ml_global_information.Init()
 		ml_mesh_mgr.SetDefaultMesh(1221, "Interphos", true)
 		ml_mesh_mgr.SetDefaultMesh(1222, "Skydeep Cenote Inner Chamber", true)
 		ml_mesh_mgr.SetDefaultMesh(1254, "[Quest] Yuweyawata", true)
+		ml_mesh_mgr.SetDefaultMesh(1268, "Break Room", true)
 	
 				
 	end
